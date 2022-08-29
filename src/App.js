150e9992e1701c1a6fd55ce47ae53498c38ec877
minoka5105 <61936313+minoka5105@users.noreply.github.com>
2022-08-29 19:23:42 +1000

    Cleaning up files

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
-import React from 'react'
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+    <div className='App'>
+     <h1>Test12313123</h1>
     </div>
+    
   );
 }
 
 export default App;
-//Azure Test
-//Github Test
