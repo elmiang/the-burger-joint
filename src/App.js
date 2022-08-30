d6b9fc3f0fbc2c7b86dae66b850cdeeba45d8353
Krobu5 <61936313+minoka5105@users.noreply.github.com>
2022-08-30 16:02:08 +1000

    Card Component Testing

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-     <h1>Test12313123</h1>
+     <h1>Restauraunt Ordering System</h1>
+    
+  <div class="card">
+    <img src="Placeholder.png" class="card-img-top" alt="..."></img>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
     </div>
+
+    
+  </div>
+    
     
   );
 }
