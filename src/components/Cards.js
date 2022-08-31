fe082243bd09d1aac24bf9d43d6fa9291c44954a
minoka5105 <61936313+minoka5105@users.noreply.github.com>
2022-08-31 19:30:37 +1000

    Updates to Card.js + added comments

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,8 +10,8 @@ const Cards = () => {
                 <div class="card h-100 w-100">
                     <img src="./Placeholder.png" class="card-img-top" alt="..."></img>
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a short card.</p>
+                            <h5 class="card-title">Title</h5>
+                            <p class="card-text">Food Information</p>
                             <p class="fw-bold">$ Price</p>
                             <button type="button" class="btn btn-secondary">Add to Cart</button>
                         </div>
@@ -23,8 +23,8 @@ const Cards = () => {
                 <div class="card h-100 w-100">
                     <img src="./Placeholder.png" class="card-img-top" alt="..."></img>
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a short card.</p>
+                            <h5 class="card-title">Title</h5>
+                            <p class="card-text">Food Information</p>
                             <p class="fw-bold">$ Price</p>
                             <button type="button" class="btn btn-secondary">Add to Cart</button>
                         </div>
@@ -36,8 +36,8 @@ const Cards = () => {
                 <div class="card h-100 w-100">
                     <img src="./Placeholder.png" class="card-img-top" alt="..."></img>
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a short card.</p>
+                            <h5 class="card-title">Title</h5>
+                            <p class="card-text">Food Information</p>
                             <p class="fw-bold">$ Price</p>
                             <button type="button" class="btn btn-secondary">Add to Cart</button>
                         </div>
@@ -49,8 +49,8 @@ const Cards = () => {
                 <div class="card h-100 w-100">
                     <img src="./Placeholder.png" class="card-img-top" alt="..."></img>
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a short card.</p>
+                            <h5 class="card-title">Title</h5>
+                            <p class="card-text">Food Information</p>
                             <p class="fw-bold">$ Price</p>
                             <button type="button" class="btn btn-secondary">Add to Cart</button>
                         </div>
