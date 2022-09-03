bb8196d7fd674b4cee7d6b0c8ed3c787ea600e66
eldermiang <61782036+eldermiang@users.noreply.github.com>
2022-09-03 18:52:36 +1000

    patch ordering option buttons

--- frontend/src/components/OrderIngredient.js
+++ frontend/src/components/OrderIngredient.js
@@ -1,12 +1,14 @@
 const OrderIngredient = (props) => {
   return (
     <div className="form-check py-2">
-      <input className="form-check-input" type="checkbox" value="" checked/>
+      <input className="form-check-input" type="checkbox" defaultChecked/>
         <label className="form-check-label ms-1 text-white">
           {props.name}
         </label>
     </div>  
   )
+
 }
+
 
 export default OrderIngredient;
