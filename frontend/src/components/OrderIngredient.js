dfef6532140be40a5c5cce199ea0616b14031aaa
eldermiang <61782036+eldermiang@users.noreply.github.com>
2022-09-03 19:14:15 +1000

    Update cart layout and patch order options

--- frontend/src/components/OrderIngredient.js
+++ frontend/src/components/OrderIngredient.js
@@ -1,7 +1,7 @@
 const OrderIngredient = (props) => {
   return (
     <div className="form-check py-2">
-      <input className="form-check-input" type="checkbox" value="" checked/>
+      <input className="form-check-input" type="checkbox" value="" defaultChecked/>
         <label className="form-check-label ms-1 text-white">
           {props.name}
         </label>
