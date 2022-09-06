6e5898130ef29acd40c82242a2025eb3c3c58953
RiverKid99 <nicholasharrison747@gmail.com>
2022-09-06 15:59:38 +1000

    Update CheckoutBar.js
    Fixed Sizing

--- frontend/src/components/CheckoutBar.js
+++ frontend/src/components/CheckoutBar.js
@@ -4,7 +4,7 @@ import React from "react";
 const CheckoutBar = () => {
     return ( 
         <div class="container-fluid">
-            <div id="sidebar" class="d-flex flex-column gap-2 gx-5 col-xl-2 pt-5 p-3 text-center list-group">
+            <div id="sidebar" class="d-flex flex-column gap-2 gx-5 col-xl-1 pt-5 p-3 text-center list-group">
                 <a class="list-group-item list-group-item-action" href="#CheckoutItems">Cart Items</a>
                 <a class="list-group-item list-group-item-action" href="#PaymentInput">Payment Details</a>
                 <a class="list-group-item list-group-item-action" href="#AddressInput">Address</a>
