0f5bfa4ee4db3f3fd0391f3c28ddf74964fbd536
MichaelScovell <michael.scovell@cevo.com.au>
2022-09-27 11:59:29 +1000

    Account Management init component commit

---
+++ frontend/src/components/OrderHistory.js
@@ -0,0 +1,3 @@
+// Order History Component
+// Component will display the order history of a user's account
+// Here they will be able view it
---
+++ frontend/src/components/ProfileDetails.js
@@ -0,0 +1,5 @@
+// Profile Details Component
+// Component will contain an form that will be populated with
+// account details of the user
+// Will have options to enable the user to:
+// Edit and Delete their account
