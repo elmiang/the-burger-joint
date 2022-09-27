2c8c0788dc38fe71a6556cb43a2dc2a6b0ec4b38
eldermiang <61782036+eldermiang@users.noreply.github.com>
2022-09-27 14:43:36 +1000

    Debug orderOptions, add persistent order options and link everything to cart items

---
+++ frontend/src/redux/cart.js
@@ -0,0 +1,38 @@
+import { createSlice, current } from '@reduxjs/toolkit'
+import React, { onEffect } from 'react';
+
+const initialState = JSON.parse(localStorage.getItem("cartItems"));
+
+//Use return for methods that directly manipulates the state
+//Use 'state = ...' for methods that update the state's properties
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    setItems(state, action) {
+      return action.payload;
+    },
+    deleteItem(state, action) {
+      const items = state.filter(item => item.id !== action.payload);
+      return items;
+    },
+    updateItemQuantity(state, action) {
+      const items = [...state];
+      items.find(item => item.id === action.payload.id).quantity = action.payload.num;
+      state = items;
+    },
+    updateItemPrice(state, action) {
+      const items = [...state];
+      items.find(item => item.id === action.payload.id).price = action.payload.num;
+      state = items;
+    },
+    updateItemExtras(state, action) {
+      const items = [...state];
+      items.find(item => item.id === action.payload.id).extra = action.payload.extra;
+      state = items;
+    }
+  },
+})
+
+export const { setItems, deleteItem, updateItemQuantity, updateItemPrice, updateItemExtras } = cartSlice.actions
+export default cartSlice.reducer
