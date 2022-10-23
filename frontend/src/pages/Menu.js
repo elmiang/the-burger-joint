83370c77192a646f285e1ac7bf0330ef193bdf00
eldermiang <61782036+eldermiang@users.noreply.github.com>
2022-10-23 19:21:59 +1100

    Debug filter

--- frontend/src/pages/Menu.js
+++ frontend/src/pages/Menu.js
@@ -42,7 +42,7 @@ const filterDishes = (dishes, query) => {
     const [searchQuery, setSearchQuery] = useState(query || '');
     const filteredDishes = filterDishes(dishes, searchQuery);
 
-    console.log(burger);
+    console.log(filteredDishes);
     //Seperating "dishes" array into three categories
     const burger = filteredDishes.filter(obj => {
         return obj.Category === 'Burger';
